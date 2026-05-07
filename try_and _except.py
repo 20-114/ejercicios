c75e8c31d65570919e10817a24396c5fcbceb33a
rafa <rafael.est07@gmail.com>
2026-05-07 11:09:47 -0400

    ejercicio_try_except 1.1

--- try_and _except.py
+++ try_and _except.py
@@ -1,1 +1,22 @@
+'''Fabrica de enlatados
+Se necesita hacer el algoritmo de productos enlatados
+Se debe consultar el peso del producto (en gramos) (solo valores positivos)
+El porcentaje de sodio en el (solo valores entre 1 y 100)
+y si se va a vender nacional o internacionalmente
+
+Considerar los criterios en la siguiente tabla
+
+menos de 500gr -> lata normal
+más de 500 hasta 1500gr -> lata mediamás de 1500gr -> lata grande
+si el Na es menos del 5% -> lata queda igual
+si Na esta entre 5 y 8% -> lata especial
+si tiene 9 o más -> lata acorazada
+A las latas internacionales se les debe pegar un sticker de validación sanitaria
+
+Ej: 800gr, 7%, 2(internacional) -> lata mediana especial con sticker sanitario
+
+
+'''
+
+
 

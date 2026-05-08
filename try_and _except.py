93bbf2e10ce5df5e8c1f9481cd19067ad5b3ac72
rafa <rafael.est07@gmail.com>
2026-05-08 10:27:28 -0400

    try_except 1.2

--- try_and _except.py
+++ try_and _except.py
@@ -14,9 +14,47 @@ si tiene 9 o más -> lata acorazada
 A las latas internacionales se les debe pegar un sticker de validación sanitaria
 
 Ej: 800gr, 7%, 2(internacional) -> lata mediana especial con sticker sanitario
-
-
 '''
+try:
+    peso_producto = int(input("Ingrese el peso en gramos: "))
+    while peso_producto <0:
+        if peso_producto < 0:
+            print("Solo puede ingresar números positivos")
+            peso_producto = int(input("Ingrese el peso: "))
+except ValueError:
+    print("Solo se pueden ingresar números")
+
+
+metodo_de_venta = ""
+try:
+    venta = int(input('''
+Indica si el producto se vendera de manera nacional o internacional:
+1. Nacional
+2. Internacional
+'''))
+    while 1 > venta > 2:
+        if venta < 1:
+            print("Solo puede ingresar 1 o 2")
+            venta = int(input('''
+Indica si el producto se vendera de manera nacional o internacional:
+1. Nacional
+2. Internacional
+'''))
+        elif venta > 2:
+            print("Solo puede ingresar 1 o 2")
+            venta = int(input('''
+Indica si el producto se vendera de manera nacional o internacional:
+1. Nacional
+2. Internacional
+'''))
+    match venta:
+        case 1:
+            metodo_de_venta = "Nacional"
+        case 2:
+            metodo_de_venta = "Internacional"
+except TypeError:
+    print("Solo puede ingresar números, 1 o 2")
+
 
 
 
